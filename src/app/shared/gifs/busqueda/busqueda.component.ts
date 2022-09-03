2c8b9baaea9d58559b3cce84822c73ced52b239e
Lucero Raul <luceroraul12@gmail.com>
2022-09-03 09:12:36 -0300

    uso del viewchild para manipular un input sin formularios ni nada

--- src/app/shared/gifs/busqueda/busqueda.component.ts
+++ src/app/shared/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent implements OnInit {
 
+  @ViewChild('txtBusqueda') txtBusqueda!: ElementRef<HTMLInputElement>;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar(){
+    alert(this.txtBusqueda.nativeElement.value);
+    this.txtBusqueda.nativeElement.value = "";
   }
 
 }
