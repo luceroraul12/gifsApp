f422c8ce8e9102db04a501cef71d75d213f7055e
Lucero Raul <luceroraul12@gmail.com>
2022-09-03 09:34:35 -0300

    add ngfor para mostrar historial

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  get listaDeBusquedas(){
+    return this.gifService.historial;
+  }
+
+  constructor(private gifService: GifsService) { }
 
   ngOnInit(): void {
   }
