a56598df69afcfb0195c1225d8697abbf00a82c7
Lucero Raul <luceroraul12@gmail.com>
2022-09-03 09:21:11 -0300

    add service

---
+++ src/app/shared/gifs/services/gifs.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _historialBusqueda: string[] = [];
+
+  get historial(){
+    return [...this._historialBusqueda];
+  }
+
+  agregarBusqueda(busqueda: string): void{
+    this._historialBusqueda.push(busqueda);
+  }
+  
+}
